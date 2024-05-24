6718383db2bfad3e9d59d3aaa5037bf9c0f24118
SuryaPain <160389278+SuryaPain@users.noreply.github.com>
2024-05-24 14:14:30 +0530

    Add files via upload

---
+++ Timestamp - Resubmission.py
@@ -0,0 +1,11 @@
+#1a
+from datetime import datetime
+dt = datetime.now()
+ts = datetime.timestamp(dt)
+print("month and time is:", dt)
+print("Timestamp is:", ts)
+
+#1b
+import time
+Surya=time.time()
+print("timestamp:",Surya)
